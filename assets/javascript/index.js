294705cc4c8a3a930c20c1abd61c28af115f1ad3
rickiangela <rickiangela@gmail.com>
2019-01-26 19:17:02 -0500

    splash page

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,68 +1,15 @@
-setTimeout(function(){window.location.href='portfolio.html'},13020);
+var type = document.getElementById('type-here');
 
-class TypeWriter {
-    constructor(txtElement, words, wait = 2000) {
-      this.txtElement = txtElement;
-      this.words = words;
-      this.txt = '';
-      this.wordIndex = 0;
-      this.wait = parseInt(wait, 10);
-      this.type();
-      this.isDeleting = false;
-    }
-  
-    type() {
-      // Current index of word
-      const current = this.wordIndex % this.words.length;
-      // Get full text of current word
-      const fullTxt = this.words[current];
-  
-      // Check if deleting
-      if(this.isDeleting) {
-        // Remove char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-      } else {
-        // Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-      }
-  
-      // Insert txt into element
-      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-  
-      // Initial Type Speed
-      let typeSpeed = 200;
-  
-      if(this.isDeleting) {
-        typeSpeed /= 2;
-      }
-  
-      // If word is complete
-      if(!this.isDeleting && this.txt === fullTxt) {
-        // Make pause at end
-        typeSpeed = this.wait;
-        // Set delete to true
-        this.isDeleting = true;
-      } else if(this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        // Move to next word
-        this.wordIndex++;
-        // Pause before start typing
-        typeSpeed = 400;
-      }
-  
-      setTimeout(() => this.type(), typeSpeed);
-    }
-  }
-  
-  
-  // Init On DOM Load
-  document.addEventListener('DOMContentLoaded', init);
-  
-  // Init App
-  function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
-    new TypeWriter(txtElement, words, wait);
-  }
+var typewriter = new Typewriter(type, {
+    loop: false
+});
+
+typewriter.typeString('\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'+'<h2')
+    .pauseFor(1000)
+    .deleteChars(1)
+    .typeString('1>Helllo ')
+    .pauseFor(1000)
+    .deleteChars(3)
+    .typeString('o World!</h1>')
+    .pauseFor(2500)
+    .start();
